75f7dbf01ca890c1ee1fd1700b51b60ffbc603c6
TalhaProgrammer92 <talha.code92@gmail.com>
2025-02-16 07:10:42 +0500

    update crud

--- WPF/CRUD App/CRUD App/obj/Release/net8.0-windows/CRUD App.AssemblyInfo.cs
+++ WPF/CRUD App/CRUD App/obj/Release/net8.0-windows/CRUD App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUD App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52727dc9a023d43f3567d9de58d63bf670340f35")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUD App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUD App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
